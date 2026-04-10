e748c703594e88802c2bd213dca9bb6a85cdfccf
pierre guegan <pierre.guegan@e-artsup.net>
2026-04-10 12:14:52 +0200

    Cors pour connexion backend frontend

--- src/main/java/com/project/pinter/controller/PinController.java
+++ src/main/java/com/project/pinter/controller/PinController.java
@@ -1,10 +1,12 @@
 package com.project.pinter.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/pins")
 public class PinController {
