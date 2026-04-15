f30120f6429354b6f6980d974eca4467bf2b6d23
pierre guegan <pierre.guegan@e-artsup.net>
2026-04-15 16:26:03 +0200

    début mise en place entities

---
+++ src/main/java/com/project/pinter/entities/User.java
@@ -0,0 +1,26 @@
+package com.project.pinter.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
