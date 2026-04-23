a130c8176f15ca891e00355ed11b7eb621e47fd7
pierre guegan <pierre.guegan@e-artsup.net>
2026-04-23 16:14:50 +0200

    get fonctionnel

--- src/main/java/com/project/pinter/entities/Image.java
+++ src/main/java/com/project/pinter/entities/Image.java
@@ -1,10 +1,15 @@
 package com.project.pinter.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 public class Image {
 
@@ -23,8 +28,11 @@ public class Image {
 
     private LocalDateTime createdAt = LocalDateTime.now();
 
+
+
     @ManyToOne
     @JoinColumn(name = "owner_id")
+    @JsonIgnore
     private User owner;
 
     private String path;
