91d3ea97f7db4f25131b2fc4ab2e9c926066bede
pierre guegan <pierre.guegan@e-artsup.net>
2026-05-11 16:53:37 +0200

    affichage image dans feed

--- src/main/java/com/project/pinter/config/WebConfig.java
+++ src/main/java/com/project/pinter/config/WebConfig.java
@@ -1,14 +1,17 @@
 package com.project.pinter.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
+
+        registry
+                .addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
     }
 }
