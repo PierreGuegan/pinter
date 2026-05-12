7515b83e8ff73ece37aae7b379f50bab9bd53ae3
pierre guegan <pierre.guegan@e-artsup.net>
2026-05-12 13:56:30 +0200

    config pour autorisation vps

--- src/main/java/com/project/pinter/config/SecurityConfig.java
+++ src/main/java/com/project/pinter/config/SecurityConfig.java
@@ -34,7 +34,10 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "http://217.160.247.93"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
