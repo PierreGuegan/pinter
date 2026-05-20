219833db7ed0a6468cfc6b12c5e50acbbb7d6bf4
pierre guegan <pierre.guegan@e-artsup.net>
2026-05-20 19:40:19 +0200

    affichage profil

--- src/main/java/com/project/pinter/controller/AuthController.java
+++ src/main/java/com/project/pinter/controller/AuthController.java
@@ -3,7 +3,10 @@ package com.project.pinter.controller;
 import com.project.pinter.dto.AuthResponse;
 import com.project.pinter.dto.LoginRequest;
 import com.project.pinter.dto.RegisterRequest;
+import com.project.pinter.entities.User;
+import com.project.pinter.repositories.UserRepository;
 import com.project.pinter.services.AuthService;
+import com.project.pinter.services.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,12 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/register")
     public void register(@RequestBody RegisterRequest req) {
@@ -27,6 +36,12 @@ public class AuthController {
         return new AuthResponse(token);
     }
 
+    @GetMapping("/me")
+    public User me(@RequestHeader("Authorization") String token) {
+        String email = jwtService.extractEmail(token.replace("Bearer ", ""));
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 
 
 
