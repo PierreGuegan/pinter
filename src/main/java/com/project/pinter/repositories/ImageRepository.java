436812eabd49cc4df910920906f7b30c3cba39a6
pierre guegan <pierre.guegan@e-artsup.net>
2026-05-21 23:16:03 +0200

    search oubli query

--- src/main/java/com/project/pinter/repositories/ImageRepository.java
+++ src/main/java/com/project/pinter/repositories/ImageRepository.java
@@ -9,6 +9,7 @@ import com.project.pinter.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 import java.util.UUID;
 
